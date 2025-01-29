c62bf3a9078d75ed4e90e25632ad6a335237935c
batraktar <maks.batrak1222@gmail.com>
2025-01-30 01:17:25 +0200

    fixed logo and font-size learning css? its beeeeeeee

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -9,23 +9,15 @@
 (function() {
   "use strict";
 
+
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
-  function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
 
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
-
-  /**
-   * Mobile nav toggle
-   */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+    /**
+     * Mobile nav toggle
+     */
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
   function mobileNavToogle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
@@ -126,5 +118,6 @@
       updateCountDown(countDownItem);
     }, 1000);
   });
+
 
 })();

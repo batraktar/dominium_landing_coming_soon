fd1560a5c3ba3902af2cdff5dc6d8de886b21d79
batraktar <maks.batrak1222@gmail.com>
2025-01-31 02:18:30 +0200

    css text, font? contacs, action

--- landing_doominium_real_state/views.py
+++ landing_doominium_real_state/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+# def action_for_owners(request):
+#     return render(request, 'action_for_owners.html')
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,123 +1,121 @@
 /**
-* Template Name: ComingSoon
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: ComingSoon
+ * Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
+ * Updated: Aug 07 2024 with Bootstrap v5.3.3
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 
-(function() {
-  "use strict";
+(function () {
+    "use strict";
 
 
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
+    /**
+     * Apply .scrolled class to the body as the page is scrolled down
+     */
 
     /**
      * Mobile nav toggle
      */
     const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
-      }
-    });
-
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
-
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
-
-  /**
-   * Scroll top button
-   */
-  let scrollTop = document.querySelector('.scroll-top');
-
-  function toggleScrollTop() {
-    if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
     }
-  }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+
+    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+    /**
+     * Hide mobile nav on same-page/hash links
+     */
+    document.querySelectorAll('#navmenu a').forEach(navmenu => {
+        navmenu.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToogle();
+            }
+        });
+
     });
-  });
 
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
+    /**
+     * Toggle mobile nav dropdowns
+     */
+    // document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    //   navmenu.addEventListener('click', function(e) {
+    //     e.preventDefault();
+    //     this.parentNode.classList.toggle('active');
+    //     this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+    //     e.stopImmediatePropagation();
+    //   });
+    // });
 
-  /**
-   * Animation on scroll function and init
-   */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
+    /**
+     * Preloader
+     */
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove();
+        });
+    }
+
+    /**
+     * Scroll top button
+     */
+    let scrollTop = document.querySelector('.scroll-top');
+
+    function toggleScrollTop() {
+        if (scrollTop) {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+        }
+    }
+
+    scrollTop.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     });
-  }
-  window.addEventListener('load', aosInit);
 
-  /**
-   * Countdown timer
-   */
-  function updateCountDown(countDownItem) {
-    const timeleft = new Date(countDownItem.getAttribute('data-count')).getTime() - new Date().getTime();
+    window.addEventListener('load', toggleScrollTop);
+    document.addEventListener('scroll', toggleScrollTop);
 
-    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    /**
+     * Animation on scroll function and init
+     */
+    function aosInit() {
+        AOS.init({
+            duration: 600,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        });
+    }
 
-    countDownItem.querySelector('.count-days').innerHTML = days;
-    countDownItem.querySelector('.count-hours').innerHTML = hours;
-    countDownItem.querySelector('.count-minutes').innerHTML = minutes;
-    countDownItem.querySelector('.count-seconds').innerHTML = seconds;
+    window.addEventListener('load', aosInit);
 
-  }
+    document.addEventListener("DOMContentLoaded", function () {
+        const serviceTitles = document.querySelectorAll(".service-title");
+        const serviceCards = document.querySelectorAll(".service-card");
 
-  document.querySelectorAll('.countdown').forEach(function(countDownItem) {
-    updateCountDown(countDownItem);
-    setInterval(function() {
-      updateCountDown(countDownItem);
-    }, 1000);
-  });
+        serviceTitles.forEach((title) => {
+            title.addEventListener("mouseenter", (e) => {
+                const targetId = e.target.getAttribute("data-target");
+
+                // Прибираємо всі активні картки
+                serviceCards.forEach((card) => card.classList.remove("active"));
+
+                // Відображаємо відповідну картку
+                const activeCard = document.getElementById(targetId);
+                if (activeCard) {
+                    activeCard.classList.add("active");
+                }
+            });
+        });
+    });
 
 
 })();

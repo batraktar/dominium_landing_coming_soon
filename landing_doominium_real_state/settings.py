bfbcda66fd51dd868dded22f07fe0d952329e1e8
batraktar <maks.batrak1222@gmail.com>
2025-02-01 02:11:29 +0200

    css text, font, action

--- landing_doominium_real_state/settings.py
+++ landing_doominium_real_state/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,12 +24,11 @@ SECRET_KEY = 'django-insecure-!xwek(pq45-*u3vm4q(%itc2$ctwed+a1)jog6r28%f_ega99!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eventmanagment.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
-    'livereload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,10 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

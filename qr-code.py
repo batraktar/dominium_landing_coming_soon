9e242ab8e45fab09695261f5ee945a26d6238f07
batraktar <maks.batrak1222@gmail.com>
2025-02-05 01:44:55 +0200

    finish1?

--- qr-code.py
+++ qr-code.py
@@ -12,3 +12,13 @@ qr.make(fit=True)
 img = qr.make_image(fill="black", back_color="white")
 img.show()  # Відкриє QR-код
 img.save("dominium-qr-code.png")  # Збереже зображення
+
+
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Отримуємо шлях до поточної папки
+PROJECT_ROOT = os.path.dirname(BASE_DIR)  # Папка, де лежить manage.py
+
+print(PROJECT_ROOT)  # Це і є Application root
+
